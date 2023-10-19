f6482d3735fb6f96ddbf2353b707798c6cff05a0
Guilber Correia <guilber.correia.gc@gmail.com>
2023-10-19 14:15:24 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/credit/application/system/CreditApplicationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.credit.application.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplicationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplicationSystemApplication>(*args)
+}
